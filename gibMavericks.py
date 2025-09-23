74982eb9a9f3d6e48330d101e4c6f84c79f8d024
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2025-09-23 15:45:44 -0500

    Get all files at the Distribution, RecoveryImage, and OSInstaller endpoints

--- gibMavericks.py
+++ gibMavericks.py
@@ -16,6 +16,9 @@ BOARD_SERIAL_NUMBER="C0243070168G3M91F"
 BOARD_ID="Mac-3CBD00234E554E41"
 ROM="003EE1E6AC14"
 
+# Filthy python 2 detection
+if 2/3==0: input = raw_input
+
 class gibMavericks:
 
     def __init__(self):
@@ -34,14 +37,13 @@ class gibMavericks:
             print(" - Failed: {}".format(e))
             exit(1)
 
-    def main(self):
-        # Walk through the steps to gather info, and document what we're doing
-        # along the way
-        print("Generating client ID...")
-        client_id = self.get_client_id()
-        print("Getting server ID from http://osrecovery.apple.com...")
+    def get_endpoint(self, client_id=None, endpoint="OSInstaller", output_folder=None, resume_incomplete=True):
+        if client_id is None:
+            print("Generating client ID...")
+            client_id = self.get_client_id()
+        print("Requesting server ID from http://osrecovery.apple.com...")
         server_id = self.get_server_id()
-        print("Building payload...")
+        print("Building payload for {}...".format(endpoint))
         # We need to build binary representations of a handful of hex values
         payload  = binascii.unhexlify(client_id)
         payload += binascii.unhexlify(server_id.split("~")[1])
@@ -69,42 +71,84 @@ class gibMavericks:
             key
         )
         req = Request(
-            "http://osrecovery.apple.com/InstallationPayload/OSInstaller",
+            "http://osrecovery.apple.com/InstallationPayload/{}".format(
+                endpoint
+            ),
             data=data.encode(),
             headers=headers
         )
-        print("Sending request...")
+        assets = []
         try:
             resp = urlopen(req)
             response = resp.read().decode()
+            # Check for chunklists first to avoid timing out
+            if "\nCU: " in response:
+                assets.append((
+                    response.split("\nCU: ")[1].split("\n")[0], # chunklist url
+                    response.split("\nCT: ")[1].split("\n")[0]  # chunklist token
+                ))
             # Get the asset url and token
-            asset_url = response.split("\nAU: ")[1].split("\n")[0]
-            asset_token = response.split("\nAT: ")[1].split("\n")[0]
+            assets.append((
+                response.split("\nAU: ")[1].split("\n")[0], # asset url
+                response.split("\nAT: ")[1].split("\n")[0]  # asset token
+            ))
         except Exception as e:
             print(" - Failed: {}".format(e))
             exit(1)
-        # Ensure we actually got the Mavericks download URL
-        if asset_url != "http://oscdn.apple.com/content/downloads/33/62/031-10295/gho4r94w66f5v4ujm0sz7k1m0hua68i6oo/OSInstaller/InstallESD.dmg":
-            print(" - Invalid response!")
+        if not assets:
+            print(" - No assets found at endpoint")
             exit(1)
-        print("Downloading InstallESD.dmg...")
-        headers = {
-            "Cookie": "AssetToken={}".format(asset_token)
-        }
-        out_file = os.path.join(os.path.dirname(os.path.realpath(__file__)),"InstallESD.dmg")
-        try:
-            saved_to = self.d.stream_to_file(
-                asset_url,
-                out_file,
-                headers=headers,
-                allow_resume=True
+        if output_folder is None:
+            # Set it to the Results folder next to this script
+            output_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)),"Results")
+        if os.path.isfile(output_folder):
+            print("{} already exists, and is a file.".format(outupt_folder))
+            exit(1)
+        elif not os.path.isdir(output_folder):
+            print("Creating {} folder...".format(os.path.basename(output_folder)))
+            try:
+                os.makedirs(output_folder)
+            except Exception as e:
+                print(" - Failed: {}".format(e))
+                exit(1)
+        for asset in assets:
+            asset_url,asset_token = asset
+            asset_name = os.path.basename(asset_url)
+            print("Downloading {}...".format(asset_name))
+            headers = {
+                "Cookie": "AssetToken={}".format(asset_token)
+            }
+            out_file = os.path.join(output_folder,asset_name)
+            try:
+                saved_to = self.d.stream_to_file(
+                    asset_url,
+                    out_file,
+                    headers=headers,
+                    allow_resume=resume_incomplete
+                )
+            except Exception as e:
+                print(" - Failed: {}".format(e))
+            if not saved_to:
+                print(" - Failed")
+                exit(1)
+            print("Saved to: {}".format(saved_to))
+
+    def main(self, endpoints=None, output_folder=None, resume_incomplete=True):
+        # Walk through the steps to gather info, and document what we're doing
+        # along the way
+        if endpoints is None:
+            endpoints = ("Distribution","RecoveryImage","OSInstaller")
+        print("Generating client ID...")
+        client_id = self.get_client_id()
+        for endpoint in endpoints:
+            self.get_endpoint(
+                client_id=client_id,
+                endpoint=endpoint,
+                output_folder=output_folder,
+                resume_incomplete=resume_incomplete
             )
-        except Exception as e:
-            print(" - Failed: {}".format(e))
-        if not saved_to:
-            print(" - Failed")
-            exit(1)
-        print("Saved to: {}".format(saved_to))
+        if os.name == "nt":
+            input("Press [enter] to exit...")
 
 if __name__ == "__main__":
     g = gibMavericks()
